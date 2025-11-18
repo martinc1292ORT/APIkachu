5cbce4e2d67682445b82ca7235a1719f97d93539
Max Monaco <monaco.delduca@gmail.com>
2025-11-17 22:34:38 -0300

    changes

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -59,7 +59,7 @@ export default function PokemonCard({ name, url }) {
     const color1 = TYPE_COLORS[type1] || "#333";
     const color2 = TYPE_COLORS[type2] || color1;
 
-    // Fondo dividido al 50% para que se vean claramente los dos tipos
+    
     return `linear-gradient(135deg, ${color1} 50%, ${color2} 50%)`;
   };
 
